456b5957c60a97ac4ed005f2b8116fd947eae183
Sergiy Stotskiy <sssv@i.ua>
2012-09-10 12:11:16 +0300

    Added "check_size" option are used before resizing. Added few comments to config.json

--- lib/php/model/image.class.php
+++ lib/php/model/image.class.php
@@ -23,7 +23,7 @@ class image {
         $isActive = true,
         $isClosed = false,
         $savedName;
-	protected static
+    protected static
         $allowImg = array('gif', 'png', 'jpeg', 'jpg');
 
     /**
@@ -31,11 +31,11 @@ class image {
      *
      * @param string $path_to_img
      */
-	public function __construct($path_to_img) {
+    public function __construct($path_to_img) {
         $this->data = self::getImageInfo($path_to_img);
 
         $this->isActive = (bool)$this->data;
-	}
+    }
 
     /**
      * Destructor
@@ -74,19 +74,19 @@ class image {
      * @param resource  $dest
      * @return resource       transparent gf color
      */
-	public static function restoreGifAlphaColor(&$src, &$dest) {
-		$transparentcolor = imagecolortransparent($src);
+    public static function restoreGifAlphaColor(&$src, &$dest) {
+        $transparentcolor = imagecolortransparent($src);
 
-		if($transparentcolor != -1){
-			$colorcount = imagecolorstotal($src);
-			imagetruecolortopalette($dest, true, $colorcount);
-			imagepalettecopy($dest, $src);
-			imagefill($dest, 0, 0, $transparentcolor);
-			imagecolortransparent($dest, $transparentcolor);
-		}
+        if($transparentcolor != -1){
+            $colorcount = imagecolorstotal($src);
+            imagetruecolortopalette($dest, true, $colorcount);
+            imagepalettecopy($dest, $src);
+            imagefill($dest, 0, 0, $transparentcolor);
+            imagecolortransparent($dest, $transparentcolor);
+        }
 
-		return $transparentcolor;
-	}
+        return $transparentcolor;
+    }
 
     /**
      * Restore alpha transparency for png images
@@ -94,10 +94,10 @@ class image {
      * @param resource $img
      * @return void
      */
-	public static function restoreAlphaColor(&$img) {
+    public static function restoreAlphaColor(&$img) {
         imagealphablending($img, false);
-		imagesavealpha($img, true);
-	}
+        imagesavealpha($img, true);
+    }
 
     /**
      * Get information about image
@@ -105,26 +105,26 @@ class image {
      * @param string $path_to_img
      * @return array|bool
      */
-	protected static function getImageInfo($path_to_img) {
-		$info = @getimagesize($path_to_img);
+    protected static function getImageInfo($path_to_img) {
+        $info = @getimagesize($path_to_img);
 
-		if(!$info) {
+        if(!$info) {
             return false;
         }
 
-		$info['width']  = $info[0];
-		$info['height'] = $info[1];
-		$info['src']    = $path_to_img;
+        $info['width']  = $info[0];
+        $info['height'] = $info[1];
+        $info['src']    = $path_to_img;
 
-		switch ($info[2]) {
-			case 1: $info['img'] = imagecreatefromgif($path_to_img);  break;
-			case 2: $info['img'] = imagecreatefromjpeg($path_to_img); break;
-			case 3: $info['img'] = imagecreatefrompng($path_to_img);  break;
-		}
-		unset($info[0], $info[1], $info[2], $info[3]);
+        switch ($info[2]) {
+            case 1: $info['img'] = imagecreatefromgif($path_to_img);  break;
+            case 2: $info['img'] = imagecreatefromjpeg($path_to_img); break;
+            case 3: $info['img'] = imagecreatefrompng($path_to_img);  break;
+        }
+        unset($info[0], $info[1], $info[2], $info[3]);
 
-		return $info;
-	}
+        return $info;
+    }
 
     /**
      * Get image type
@@ -140,16 +140,16 @@ class image {
      *
      * @return bool
      */
-	public function setType($type) {
-		if(!in_array($type, self::$allowImg)) {
+    public function setType($type) {
+        if(!in_array($type, self::$allowImg)) {
             return false;
         }
 
-		$mime = $this->data['mime'];
-		$this->data['mime'] = 'image/'.$type;
+        $mime = $this->data['mime'];
+        $this->data['mime'] = 'image/'.$type;
 
-		return true;
-	}
+        return true;
+    }
 
     /**
      * Prepare image filename based on $this->override
@@ -183,24 +183,24 @@ class image {
      * @param int    $chmod
      * @return string|bool    saved name or false
      */
-	public function save($name = null, $quality = 65, $chmod = 0664){
+    public function save($name = null, $quality = 65, $chmod = 0664){
         if ($this->isClosed) {
             return false;
         }
 
         $type = $this->getType();
-		$save_fn = 'image' . $type;
+        $save_fn = 'image' . $type;
         $name = trim($name);
 
         if (!function_exists($save_fn) || is_dir($name)) {
             return false;
         }
 
-		if (empty($name)) {
-			header('Content-type: '.$this->data['mime']);
+        if (empty($name)) {
+            header('Content-type: '.$this->data['mime']);
 
-			return $save_fn($this->data['img']);
-		} else {
+            return $save_fn($this->data['img']);
+        } else {
             $info = $this->getPathInfo($name);
             if (in_array($info['extension'], self::$allowImg)) {
                 $type = $info['extension'];
@@ -229,7 +229,7 @@ class image {
         } else {
             return false;
         }
-	}
+    }
 
     /**
      * Get saved name
@@ -250,67 +250,67 @@ class image {
      * @param array $bg     corners background in RGBA if $bg[4] == 127 corners will be transparent
      * @return image
      */
-	public function corners($radius = 50, $rate = 10, $bg = array(255, 255, 255, 127)){
-		$type = $this->getType();
+    public function corners($radius = 50, $rate = 10, $bg = array(255, 255, 255, 127)){
+        $type = $this->getType();
 
-		if ($rate <= 0 || $radius <= 0 || !$type || count($bg) < 4) {
+        if ($rate <= 0 || $radius <= 0 || !$type || count($bg) < 4) {
             return false;
         }
-		if ($type == 'jpeg' || $type == 'gif') {
-			$gif_alpha = $bg[3];
-			$bg[3] = 0;
-		}
-
-		$dest   = &$this->data['img'];
-		$tmp_r  = $rate * $radius;
-		$size   = 2 * $tmp_r;
-		$w = $this->data['width'];
-		$h = $this->data['height'];
-
-		self::restoreAlphaColor($this->data['img']);
-		$corners = imagecreatetruecolor($size, $size);
-		imagealphablending($corners, false);
-
-		$alpha = imagecolorallocatealpha($corners, $bg[0], $bg[1], $bg[2], (int)$bg[3]);
-		$points = array(
-			array(0,0,0,0),//[0]&[1]=Adest(0,0) [2]&[3]=Asrc(0,0)
-			array($tmp_r, 0, $w - $radius, 0),
-			array($tmp_r, $tmp_r, $w - $radius, $h - $radius),
-			array(0, $tmp_r, 0, $h - $radius)
-		);
-		$maskPoints = array(
-			array(1, 1),
-			array($size-1, $size-1),
-			array($size-1, 1),
-			array(1, $size-1)
-		);
-
-		foreach ($points as &$point) {
-			imagecopyresampled($corners, $dest, $point[0], $point[1], $point[2], $point[3], $tmp_r, $tmp_r, $radius, $radius);
+        if ($type == 'jpeg' || $type == 'gif') {
+            $gif_alpha = $bg[3];
+            $bg[3] = 0;
         }
 
-		$info = array('width' => $size,'height' => $size,'img' => $corners);
-		$this->mask('ellipse', array(
+        $dest   = &$this->data['img'];
+        $tmp_r  = $rate * $radius;
+        $size   = 2 * $tmp_r;
+        $w = $this->data['width'];
+        $h = $this->data['height'];
+
+        self::restoreAlphaColor($this->data['img']);
+        $corners = imagecreatetruecolor($size, $size);
+        imagealphablending($corners, false);
+
+        $alpha = imagecolorallocatealpha($corners, $bg[0], $bg[1], $bg[2], (int)$bg[3]);
+        $points = array(
+            array(0,0,0,0),//[0]&[1]=Adest(0,0) [2]&[3]=Asrc(0,0)
+            array($tmp_r, 0, $w - $radius, 0),
+            array($tmp_r, $tmp_r, $w - $radius, $h - $radius),
+            array(0, $tmp_r, 0, $h - $radius)
+        );
+        $maskPoints = array(
+            array(1, 1),
+            array($size-1, $size-1),
+            array($size-1, 1),
+            array(1, $size-1)
+        );
+
+        foreach ($points as &$point) {
+            imagecopyresampled($corners, $dest, $point[0], $point[1], $point[2], $point[3], $tmp_r, $tmp_r, $radius, $radius);
+        }
+
+        $info = array('width' => $size,'height' => $size,'img' => $corners);
+        $this->mask('ellipse', array(
             'x'    => $tmp_r,
             'y'    => $tmp_r,
             'width'  => $size,
             'height' => $size
         ), 100, array(14, 255, 12), $info);
-		unset($info);
+        unset($info);
 
-		foreach ($maskPoints as &$point) {
+        foreach ($maskPoints as &$point) {
             imagefill($corners, $point[0], $point[1], $alpha);
         }
         unset($maskPoints);
 
-		foreach ($points as &$point) {
+        foreach ($points as &$point) {
             imagecopyresampled($dest, $corners, $point[2], $point[3], $point[0], $point[1], $radius, $radius, $tmp_r, $tmp_r);
         }
 
-		if($type == 'gif' && $gif_alpha == 127){
-			$alpha = imagecolorat($dest, 0, 0);
-			imagecolortransparent($dest, $alpha);
-			imagesetthickness($dest, 2);
+        if($type == 'gif' && $gif_alpha == 127){
+            $alpha = imagecolorat($dest, 0, 0);
+            imagecolortransparent($dest, $alpha);
+            imagesetthickness($dest, 2);
 
             $gifPoints = array(
                 array($w - $radius + 1,$h - $radius + 1),
@@ -319,16 +319,16 @@ class image {
                 array($w - $radius + 1, $radius - 1)
             );
 
-			foreach ($gifPoints as $angle => &$point) {
+            foreach ($gifPoints as $angle => &$point) {
                 imagearc($dest, $point[0], $point[1], 2 * $radius, 2 * $radius, 90 * $angle, ($angle + 1) * 90, $alpha);
             }
         }
 
-		imagedestroy($corners);
-		unset($dest);
+        imagedestroy($corners);
+        unset($dest);
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * Resize mode
@@ -339,11 +339,11 @@ class image {
      * @param array  $options  for crop "type" (left-top, left-bottom, center, right-bottom, right-top, specific array('x' => int, 'y' => int))
      * @return array
      */
-	protected function resizeMode(&$x, &$y, $mode = 'auto', array $options = null){
-		$w = $this->data['width'];
-		$h = $this->data['height'];
+    protected function resizeMode(&$x, &$y, $mode = 'auto', array $options = null){
+        $w = $this->data['width'];
+        $h = $this->data['height'];
 
-		if($mode == 'auto') {
+        if($mode == 'auto') {
             $mode = $w >= $h ? 'width' : 'height';
         }
 
@@ -351,8 +351,8 @@ class image {
             $options['type'] = null;
         }
 
-		switch($mode){
-			case 'crop':
+        switch($mode){
+            case 'crop':
                 if ($x > $w) $x = $w;
                 if ($y > $h) $y = $h;
 
@@ -382,25 +382,25 @@ class image {
                     break;
                 }
 
-				return array($_x, $_y, $x, $y);
-			break;
-			case 'width':
-				$n = $w / $h;
-				$y = $x / $n;
-			break;
-			case 'height':
-				$n = $h / $w;
-				$x = $y / $n;
-			break;
-			default: // zoom
-				$n = $w / $h;
-				$x *= $w;
-				$y = $x / $n;
-			break;
-		}
+                return array($_x, $_y, $x, $y);
+            break;
+            case 'width':
+                $n = $w / $h;
+                $y = $x / $n;
+            break;
+            case 'height':
+                $n = $h / $w;
+                $x = $y / $n;
+            break;
+            default: // zoom
+                $n = $w / $h;
+                $x *= $w;
+                $y = $x / $n;
+            break;
+        }
 
-		return array(0, 0, $w, $h);
-	}
+        return array(0, 0, $w, $h);
+    }
 
     /**
      * Resize image
@@ -411,29 +411,29 @@ class image {
      * @param array  $options see resizeMode()
      * @return bool
      */
-	public function resize($x, $y = 1, $mode = 'auto', array $options = array()) {
-		if($x <= 0 || $y <= 0 && $mode != 'zoom') {
+    public function resize($x, $y = 1, $mode = 'auto', array $options = array()) {
+        if($x <= 0 || $y <= 0 && $mode != 'zoom') {
             return false;
         }
 
-		$type = $this->getType();
-		$w = $this->data['width'];
-		$h = $this->data['height'];
-		$pos = $this->resizeMode($x, $y, $mode, $options);
+        $type = $this->getType();
+        $w = $this->data['width'];
+        $h = $this->data['height'];
+        $pos = $this->resizeMode($x, $y, $mode, $options);
 
-		$new = imagecreatetruecolor($x, $y);
+        $new = imagecreatetruecolor($x, $y);
         $this->saveAlpha($new);
 
-		imagecopyresampled($new, $this->data['img'], 0, 0, $pos[0], $pos[1], $x, $y, $pos[2], $pos[3]);
+        imagecopyresampled($new, $this->data['img'], 0, 0, $pos[0], $pos[1], $x, $y, $pos[2], $pos[3]);
         imagedestroy($this->data['img']);
 
-		$this->data['width'] = $x;
-		$this->data['height'] = $y;
-		$this->data['img'] = $new;
-		unset($new);
+        $this->data['width'] = $x;
+        $this->data['height'] = $y;
+        $this->data['img'] = $new;
+        unset($new);
 
-		return true;
-	}
+        return true;
+    }
 
     /**
      * Mask image using one of the implemented mask methods
@@ -446,32 +446,32 @@ class image {
      * @param resource $apply_to image resource
      * @return bool|image
      */
-	public function mask($callable, $cfg, $opacity = 100, array $bg = array(0, 0, 0), $apply_to = null) {
+    public function mask($callable, $cfg, $opacity = 100, array $bg = array(0, 0, 0), $apply_to = null) {
         if (is_string($callable) && method_exists($this, strtolower($callable) . 'Mask')) {
             $callable = array($this, strtolower($callable) . 'Mask');
         }elseif (!is_callable($callable)) {
             return false;
         }
 
-		$info = is_null($apply_to) ? $this->data : $apply_to;
-		unset($apply_to);
+        $info = is_null($apply_to) ? $this->data : $apply_to;
+        unset($apply_to);
 
-		$w = $info['width'];
-		$h = $info['height'];
-		$mask = imagecreatetruecolor($w, $h);
-		$bg = imagecolorallocate($mask, $bg[0], $bg[1], $bg[2]);
-		imagefill($mask, 0, 0, $bg);
+        $w = $info['width'];
+        $h = $info['height'];
+        $mask = imagecreatetruecolor($w, $h);
+        $bg = imagecolorallocate($mask, $bg[0], $bg[1], $bg[2]);
+        imagefill($mask, 0, 0, $bg);
 
-		$alpha = imagecolorallocate($mask, 21, 243, 244);
-		imagecolortransparent($mask, $alpha);
+        $alpha = imagecolorallocate($mask, 21, 243, 244);
+        imagecolortransparent($mask, $alpha);
         if (call_user_func($callable, $mask, $alpha, $cfg, $this)) {
             imagecopymerge($info['img'], $mask, 0, 0, 0, 0, $w, $h, $opacity);
         }
         imagedestroy($mask);
-		unset($info);
+        unset($info);
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * Make image color transparent
@@ -493,16 +493,16 @@ class image {
      * @param array    $cfg    configs
      * @return void
      */
-	protected function polygonMask(&$mask, &$alpha, array &$cfg) {
-		if(!is_array($cfg)) {
-			throw new Exception('Wrong configuration for image::' . __FUNCTION__);
+    protected function polygonMask(&$mask, &$alpha, array &$cfg) {
+        if(!is_array($cfg)) {
+            throw new Exception('Wrong configuration for image::' . __FUNCTION__);
         }
 
         $cfg = array_map('floatval', $cfg);
-		$num_points = count($cfg) / 2;
-		imagefilledpolygon($mask, $cfg, $num_points, $alpha);
+        $num_points = count($cfg) / 2;
+        imagefilledpolygon($mask, $cfg, $num_points, $alpha);
         return true;
-	}
+    }
 
     /**
      * Callaback for mask() method
@@ -513,15 +513,15 @@ class image {
      * @param array    $cfg    configs
      * @return void
      */
-	protected function ellipseMask(&$mask, &$alpha, array &$cfg) {
-		if(!isset($cfg['x']) || !isset($cfg['y'])
+    protected function ellipseMask(&$mask, &$alpha, array &$cfg) {
+        if(!isset($cfg['x']) || !isset($cfg['y'])
            || !isset($cfg['width']) || !isset($cfg['height'])
         ) {
             throw new Exception('Wrong configuration for image::' . __FUNCTION__);
         }
-		imagefilledellipse($mask, $cfg['x'], $cfg['y'], $cfg['width'], $cfg['height'], $alpha);
+        imagefilledellipse($mask, $cfg['x'], $cfg['y'], $cfg['width'], $cfg['height'], $alpha);
         return true;
-	}
+    }
 
     /**
      * Rotate image
@@ -530,23 +530,23 @@ class image {
      * @param int $angle
      * @return image
      */
-	public function rotate($angle, $bg = '') {
-		$type = $this->getType();
+    public function rotate($angle, $bg = '') {
+        $type = $this->getType();
         if (!function_exists('imagerotate')) {
             throw new Exception("Your PHP does not have functionality to rotate the image");
         }
 
-		$rotate = imagerotate($this->data['img'], $angle, imagecolorallocatealpha($this->data['img'], 0, 0, 0, 127));
+        $rotate = imagerotate($this->data['img'], $angle, imagecolorallocatealpha($this->data['img'], 0, 0, 0, 127));
         $this->saveAlpha($rotate);
 
-		imagedestroy($this->data['img']);
+        imagedestroy($this->data['img']);
 
-		$this->data['img']    = &$rotate;
-		$this->data['width']  = imagesx($rotate);
-		$this->data['height'] = imagesy($rotate);
+        $this->data['img']    = &$rotate;
+        $this->data['width']  = imagesx($rotate);
+        $this->data['height'] = imagesy($rotate);
 
         return $this;
-	}
+    }
 
     /**
      * Save image alpha color
@@ -554,7 +554,7 @@ class image {
      * @param resource $resource by default using $this->data['img'] resource
      * @return image
      */
-	public function saveAlpha($resource = null){
+    public function saveAlpha($resource = null){
         if(is_null($resource)) {
             $resource = $this->data['img'];
         }
@@ -568,7 +568,7 @@ class image {
             break;
         }
         return $this;
-	}
+    }
 
     /**
      * Copy one image to another. Can be used for creating watermarks
@@ -583,9 +583,9 @@ class image {
      * @param array $cfg
      * @return image
      */
-	public function mark($path, array $cfg) {
-		$info = self::getImageInfo($path);
-		$type = $this->getType();
+    public function mark($path, array $cfg) {
+        $info = self::getImageInfo($path);
+        $type = $this->getType();
 
         if(isset($cfg['opacity'])) {
             imagecopymerge($this->data['img'], $info['img'], $cfg['x'], $cfg['y'], 0, 0, $info['width'], $info['height'], $cfg['opacity']);
@@ -597,7 +597,7 @@ class image {
         unset($info);
 
         return $this;
-	}
+    }
 
     /**
      * Clone image resource
@@ -654,7 +654,15 @@ class image {
         foreach ($options as $prefix => $config) {
             $tmb = clone $this;
             $tmb->override = $override;
-            if (isset($config['width']) && isset($config['height']) && isset($config['type'])) {
+            if (isset($config['width'])
+                && isset($config['height'])
+                && isset($config['type'])
+                && (
+                    empty($config['check_size'])
+                    || $tmb->getData('width') > $config['width']
+                    || $tmb->getData('height') > $config['height']
+                )
+            ) {
                 $tmb->resize($config['width'], $config['height'], $config['type']);
             }
             if (isset($config['crop'])) {
